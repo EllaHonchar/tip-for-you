6f3bd2054cd3b0b4e7dbe2fbd2426163810505ed
EllaHonchar <100204059+EllaHonchar@users.noreply.github.com>
2022-05-17 13:05:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import image from './tips.jpg';
 
 function App() {
+
+  const [tips, setTips] = useState("");
+
+  const fetchTips = async () => {
+  const response = await fetch("http://www.boredapi.com/api/activity/");
+
+  const data = await response.json();
+  setTips(data.activity);
+  };
+
+  useEffect(() => {
+fetchTips();
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Let's beat boredom</h1>
+      </div>
+    <div className="box">
+      <div className="block">
+        <img src={image} alt="sad"/>
+      </div>
+      <div className="tips">
+        <p> {tips} </p>
+        <button onClick={fetchTips}>New Tip</button> </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
